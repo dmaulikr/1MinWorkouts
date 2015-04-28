207c5f56122c782835b2f4f95d6d049f659a078c
Justin Mandell <Justin.Mandell@spirent.com>
2015-04-28 11:38:11 -0700

    changed start day settings page to be same as OOBE page

--- 1MinWorkouts/SettingsDetailViewController.swift
+++ 1MinWorkouts/SettingsDetailViewController.swift
@@ -8,77 +8,155 @@
 
 import UIKit
 
-class SettingsDetailViewController: UIViewController , UITableViewDataSource  {
+class SettingsDetailViewController: UITableViewController{
     
-    //var settingsSet = GlobalVars.settingsSet
+    var hour = 8
+    var min = 30
+    var weekday = true
+    var weekend = false
     
-    @IBOutlet var startDayReminderView: UIView!
-    @IBOutlet var startDayToggle: UISwitch!
+    @IBOutlet var startDaySwitch: UISwitch!
+    @IBAction func startDaySwitch(sender: AnyObject) {
+        if startDaySwitch.on{
+            datePickerCell.hidden = false
+            monFriCell.hidden = false
+            satSunCell.hidden = false
+            println("startDaySwitch is on")
+        }else{
+            datePickerCell.hidden = true
+            monFriCell.hidden = true
+            satSunCell.hidden = true
+            println("startDaySwitch is off")
+        }
+    }
+    
+    @IBOutlet var datePickerCell: UITableViewCell!
+    @IBOutlet var monFriCell: UITableViewCell!
+    @IBOutlet var satSunCell: UITableViewCell!
+    @IBOutlet var mfCheckmark: UIImageView!
+    @IBOutlet var ssCheckmark: UIImageView!
+    @IBOutlet var saveButtonCell: UITableViewCell!
+    
+    @IBAction func saveSettingsButton(sender: AnyObject) {
+        println("saveSettingsButton tapped")
+        if startDaySwitch.on{
+            println("startDaySwitch is on")
+            
+            // save the time, weekday/end settings
+            let appUserSettings = NSUserDefaults.standardUserDefaults() // instantiates a user default holder
+            
+            appUserSettings.setInteger(hour, forKey: GlobalVars.startDayHour)
+            println("startDayHour was set to \(appUserSettings.valueForKey(GlobalVars.startDayHour))")
+            
+            appUserSettings.setInteger(min, forKey: GlobalVars.startDayMin)
+            println("startDayMin was set to \(appUserSettings.valueForKey(GlobalVars.startDayMin))")
+            
+            appUserSettings.setBool(weekday, forKey: GlobalVars.notificationWeekday)
+            println("notificationWeekday was set to \(appUserSettings.valueForKey(GlobalVars.notificationWeekday))")
+            
+            appUserSettings.setBool(weekend, forKey: GlobalVars.notificationWeekend)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.notificationWeekend))")
+            
+            appUserSettings.setBool(true, forKey: GlobalVars.oobeStartDaySetup)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.oobeStartDaySetup))")
+            
+            appUserSettings.setBool(true, forKey: GlobalVars.oobeTute)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.oobeTute))")
+            
+            /*--- Need to figure out how to pop off StartDaySettings, walkthrough and Disclaimer XIB and go back to 1MW start (Main.storyboard) ---*/
+            // pops back to the root, prior to the disclaimer xib
+            let stb = UIStoryboard(name: "Main", bundle: nil)
+            let oneMWStart = stb.instantiateViewControllerWithIdentifier("oneMWTabControl") as! UINavigationController
+            
+            self.presentViewController(oneMWStart, animated: true, completion: nil)
+        }else{
+            println("startDaySwitch is off")
+            
+            //save the weekday/end settings as false
+            let appUserSettings = NSUserDefaults.standardUserDefaults() // instantiates a user default holder
+            
+            appUserSettings.setBool(false, forKey: GlobalVars.notificationWeekday)
+            println("notificationWeekday was set to \(appUserSettings.valueForKey(GlobalVars.notificationWeekday))")
+            
+            appUserSettings.setBool(false, forKey: GlobalVars.notificationWeekend)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.notificationWeekend))")
+            
+            appUserSettings.setBool(true, forKey: GlobalVars.oobeStartDaySetup)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.oobeStartDaySetup))")
+            
+            appUserSettings.setBool(true, forKey: GlobalVars.oobeTute)
+            println("notificationWeekend was set to \(appUserSettings.valueForKey(GlobalVars.oobeTute))")
+            
+            // pops back to the root, prior to the disclaimer xib
+            navigationController?.popToRootViewControllerAnimated(true)
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
     @IBOutlet var startDayPicker: UIDatePicker!
     
-    @IBOutlet var autoEndDayView: UIView!
-    @IBOutlet var autoEndDayPicker: UIDatePicker!
-    
-    @IBOutlet var aboutView: UIView!
-    
-    let settingsStartDay = [
-        ("Monday - Friday"),
-        ("Saturday and Sunday")
-    ]
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    @IBAction func startDayPIcker(sender: AnyObject) {
+        // sets the hour and min vars to whatever was changed on the startDayPIcker
+        hour = sender.hour
+        min = sender.minute
+        println("startDayPicker was touched. The Hour is \(sender.hour)" + " The Min is \(sender.minute)")
         
-        return settingsStartDay.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        var cell = tableView.dequeueReusableCellWithIdentifier("checkedCells", forIndexPath: indexPath) as! UITableViewCell
-        
-           let (settingsTitle) = settingsStartDay[indexPath.row]
-            cell.textLabel?.text = settingsTitle
-        
-        return cell
-    }
-
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        
-        let cell: UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
-        if cell.accessoryType == UITableViewCellAccessoryType.None {
-            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
-        } else {
-            cell.accessoryType = UITableViewCellAccessoryType.None
-        }
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
-        println("You selected cell #\(indexPath.row)!")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // sets the look of the weekend checkmark to off, which matches the starting var value
+        ssCheckmark.image = UIImage(named: "checkmark-off")
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if indexPath.row == 3 {
+            monFriCell.selected = false
+            
+            if mfCheckmark.image == UIImage(named: "checkmark-on"){
+                mfCheckmark.image = UIImage(named: "checkmark-off")
+                weekday = false
+                println("Mon-Fri is set to \(weekday)")
+            }else{
+                mfCheckmark.image = UIImage(named: "checkmark-on")
+                weekday = true
+                println("Mon-Fri is set to \(weekday)")
+            }
+            
+            println("Monday-Friday was tapped \(indexPath.row)")
+        }
+        if indexPath.row == 4 {
+            satSunCell.selected = false
+            
+            if ssCheckmark.image == UIImage(named: "checkmark-on"){
+                ssCheckmark.image = UIImage(named: "checkmark-off")
+                weekend = false
+                println("Sat-Sun is set to \(weekend)")
+            }else{
+                ssCheckmark.image = UIImage(named: "checkmark-on")
+                weekend = true
+                println("Sat-Sun is set to \(weekend)")
+            }
+            
+            println("Saturday and Sunday was tapped \(indexPath.row)")
+        }
+    }
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
